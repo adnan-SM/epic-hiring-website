1b586f37cf38e062021f908da7a7fdf46cf2282b
Adnan A M <adnan.tlkn@gmail.com>
2021-05-30 23:41:54 +0530

    Added missing alt tags

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <div>
-            <img src='https://svgshare.com/i/QZX.svg' title='' />
+            <img src='https://svgshare.com/i/QZX.svg' title='' alt='Girl sitting under a lamp'/>
         </div>
     );
 }
