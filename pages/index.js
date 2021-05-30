1d3d852eb76cadd065015e0bf85be27c41fe1500
Adnan A M <adnan.tlkn@gmail.com>
2021-05-30 23:48:14 +0530

    Updated title

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function IndexPage() {
           <Head>
               <title>Epic Hiring Course</title>
               <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-              <meta property="og:title" content="Epic Hiring Course" key="title" />
+              <meta property="og:title" content="Ace your next interview | Epic Hiring Course" key="title" />
               <meta name="description" content="Learn how to ace your interview & negotiate better salaries" />
               <meta property="og:description" content="Learn how to ace your interview & negotiate better salaries" />
           </Head>
